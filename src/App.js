98e1507bcfa312ff443555d310f26207a529875c
MIhir Bagga <62807580+mihirbagga@users.noreply.github.com>
2020-07-10 12:07:01 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Food from './food';
+import Pokegame from './Pokemon/Pokegame';
+
+
+function App() {
+  return (
+    <div >
+     
+     <Pokegame/>
+    </div>
+  );
+}
+
+export default App;
